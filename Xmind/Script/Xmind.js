d3200ab90dada7b2163de2990bf95e98c380371a
Xians <github@xians.su>
2022-09-01 11:59:31 +0800

    Update Xmind.js

--- Xmind/Script/Xmind.js
+++ Xmind/Script/Xmind.js
@@ -1,13 +1,13 @@
 /*
-脚本功能：解锁Xmind思维导图订阅到2030年
-脚本作者：R·E
-脚本发布：https://t.me/Functional_Store_Hub
+腳本功能：解鎖Xmind思維導圖訂閱到2030年
+腳本作者：R·E
+腳本發布：https://t.me/Functional_Store_Hub
 
-更新时间：2022.08.13
-支持版本：商店版本 
-下载地址：https://apps.apple.com/app/id1286983622
+更新時間：2022.08.13
+支援版本：商店版本 
+下載地址：https://apps.apple.com/app/id1286983622
 
-使用声明：⚠️⚠️⚠️此脚本仅供学习与交流,禁止转载与贩卖,需在24小时内从设备中删除！⚠️⚠️⚠️
+使用聲明：⚠️⚠️⚠️此腳本僅供學習及交流,禁止轉載與販賣,需在24小時內從設備中刪除！⚠️⚠️⚠️
 */
 
 var _0x49754b=JSON['parse']($response['body']);_0x49754b['license']['status']='sub';_0x49754b['license']['expireTime']=0x1b8d90e4000;$done({'body':JSON['stringify'](_0x49754b)});
